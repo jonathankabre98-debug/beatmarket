3ac09a637eebf670d2f511f71e1e8dba0bbe1046
jonathankabre98-debug <jonathankabre98@gmail.com>
2026-07-08 21:09:08 +0000

    Fix import path for BeatMarket component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../BeatMarket.jsx'
+import App from './BeatMarket.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
